ab80ad2275bfc07efd4efbc6a0aab5cb0d445418
Mohammed Abdel Raouf <mohammed@raouf.me>
2022-05-05 15:58:47 +0200

    update result app's node version and use alpine docker image, start using yarn instead of node

--- result/server.js
+++ result/server.js
@@ -1,43 +1,41 @@
-var express = require('express'),
-    async = require('async'),
-    pg = require('pg'),
-    { Pool } = require('pg'),
-    path = require('path'),
-    cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override'),
-    app = express(),
-    server = require('http').Server(app),
-    io = require('socket.io')(server);
+const async = require('async');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const http = require('http');
+const methodOverride = require('method-override');
+const { Pool } = require('pg')
+const path = require('path');
+const socket = require('socket.io')
 
+const port = 80;
+const app = express();
+const server = http.Server(app);
+const io = socket(server)
 io.set('transports', ['polling']);
-
-var port = process.env.PORT || 4000;
 
 io.sockets.on('connection', function (socket) {
 
-  socket.emit('message', { text : 'Welcome!' });
+  socket.emit('message', { text: 'Welcome!' });
 
   socket.on('subscribe', function (data) {
     socket.join(data.channel);
   });
 });
 
-var pool = new pg.Pool({
-  connectionString: 'postgres://postgres:postgres@db/postgres'
-});
+const pool = new Pool({ connectionString: 'postgres://postgres:password@db/postgres' });
 
 async.retry(
   {times: 1000, interval: 1000},
-  function(callback) {
-    pool.connect(function(err, client, done) {
+  function (callback) {
+    pool.connect(function (err, client) {
       if (err) {
         console.error("Waiting for db");
       }
       callback(err, client);
     });
   },
-  function(err, client) {
+  function (err, client) {
     if (err) {
       return console.error("Giving up");
     }
@@ -47,20 +45,22 @@ async.retry(
 );
 
 function getVotes(client) {
-  client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function(err, result) {
+  client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function (err, result) {
     if (err) {
       console.error("Error performing query: " + err);
     } else {
-      var votes = collectVotesFromResult(result);
+      const votes = collectVotesFromResult(result);
       io.sockets.emit("scores", JSON.stringify(votes));
     }
 
-    setTimeout(function() {getVotes(client) }, 1000);
+    setTimeout(function () {
+      getVotes(client)
+    }, 1000);
   });
 }
 
 function collectVotesFromResult(result) {
-  var votes = {a: 0, b: 0};
+  const votes = {a: 0, b: 0};
 
   result.rows.forEach(function (row) {
     votes[row.vote] = parseInt(row.count);
@@ -70,9 +70,10 @@ function collectVotesFromResult(result) {
 }
 
 app.use(cookieParser());
-app.use(bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
@@ -86,6 +87,6 @@ app.get('/', function (req, res) {
 });
 
 server.listen(port, function () {
-  var port = server.address().port;
+  const port = server.address().port;
   console.log('App running on port ' + port);
 });
--- result/views/app.js
+++ result/views/app.js
@@ -1,43 +1,43 @@
-var app = angular.module('catsvsdogs', []);
-var socket = io.connect({transports:['polling']});
+const app = angular.module('catsvsdogs', []);
+const socket = io.connect({ transports: ['polling']} );
 
-var bg1 = document.getElementById('background-stats-1');
-var bg2 = document.getElementById('background-stats-2');
+const bg1 = document.getElementById('background-stats-1');
+const bg2 = document.getElementById('background-stats-2');
 
-app.controller('statsCtrl', function($scope){
+app.controller('statsCtrl', function ($scope) {
   $scope.aPercent = 50;
   $scope.bPercent = 50;
 
-  var updateScores = function(){
+  const updateScores = function () {
     socket.on('scores', function (json) {
-       data = JSON.parse(json);
-       var a = parseInt(data.a || 0);
-       var b = parseInt(data.b || 0);
+      const data = JSON.parse(json);
+      const a = parseInt(data.a || 0);
+      const b = parseInt(data.b || 0);
 
-       var percentages = getPercentages(a, b);
+      const percentages = getPercentages(a, b);
 
-       bg1.style.width = percentages.a + "%";
-       bg2.style.width = percentages.b + "%";
+      bg1.style.width = percentages.a + "%";
+      bg2.style.width = percentages.b + "%";
 
-       $scope.$apply(function () {
-         $scope.aPercent = percentages.a;
-         $scope.bPercent = percentages.b;
-         $scope.total = a + b;
-       });
+      $scope.$apply(function () {
+        $scope.aPercent = percentages.a;
+        $scope.bPercent = percentages.b;
+        $scope.total = a + b;
+      });
     });
   };
 
-  var init = function(){
-    document.body.style.opacity=1;
+  const init = function () {
+    document.body.style.opacity = "1";
     updateScores();
   };
-  socket.on('message',function(data){
+  socket.on('message', function () {
     init();
   });
 });
 
 function getPercentages(a, b) {
-  var result = {};
+  const result = {};
 
   if (a + b > 0) {
     result.a = Math.round(a / (a + b) * 100);
